67cad0db2473452f73746e157ef25c76ada4dc7c
alicekananda <alicekananda@gmail.com>
2021-09-03 18:22:59 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(
+    Center(
+      child: Text(
+        "Olá Alice",
+        textDirection: TextDirection.ltr,
+      ),
+    )
+  );
+}
