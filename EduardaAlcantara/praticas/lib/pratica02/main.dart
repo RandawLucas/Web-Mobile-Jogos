ac51f3236bc150a9b7ed48299031b154240a1a11
Eduarda Alcantara <eduardaalc@outlook.com>
2021-09-06 22:15:12 -0300

    Adicionando pasta

---
+++ EduardaAlcantara/praticas/lib/pratica02/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        'Eduarda Alcântara',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
