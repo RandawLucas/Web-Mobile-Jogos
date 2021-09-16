dbf02ad966565b784ce2b47e833e467146078579
RandawLucas <r.randawlucas@hotmail.com>
2021-09-16 19:58:27 -0300

    adicionando ou modificação das praticas 07 e 09

--- RandawLucas/projetos/lib/pratica07/main.dart
+++ RandawLucas/projetos/lib/pratica07/main.dart
@@ -3,18 +3,15 @@ void main() {
  runApp(
   Center(
     child: Container(
-      child: Container(
-        padding: EdgeInsets.all(3),
-        alignment: Alignment.center,
-        child: Image.network(
-              'https://picsum.photos/250?image=37'),
+      child: Image.network(
+            'https://picsum.photos/250?image=37'
       ),
       decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.yellow,
+      shape: BoxShape.circle,
+      color: Colors.yellow,
       ),
-    padding: EdgeInsets.all(40),
-    margin: const EdgeInsets.all(25),
+      padding: EdgeInsets.all(40),
+      margin: const EdgeInsets.all(25),
     ),
   ),
  );
--- RandawLucas/projetos/lib/pratica09/main.dart
+++ RandawLucas/projetos/lib/pratica09/main.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(home: Home()));
 class Home extends StatefulWidget {
   @override
-    HomeState createState() {
-    return HomeState();
- }
+    HomeState createState() => HomeState();
+ 
 }
 class HomeState extends State<Home> {
   int numeroVezes = 0;
   String mensagem = "Número de vezes em que o botão foi pressionado: 0";
   void cliqueDoBotao() {
-  numeroVezes = numeroVezes + 1;
+  numeroVezes += 1;
   mensagem = "Número de vezes em que o botão foi pressionado: $numeroVezes \nNúmero atual é ${numeroVezes%2==0 ? "par" : "ímpar"}";
  }
  @override
@@ -27,12 +26,12 @@ class HomeState extends State<Home> {
       ],
     ),
   ),
- floatingActionButton: FloatingActionButton(
- onPressed: () {
- setState(cliqueDoBotao);
- },
- child: Icon(Icons.add),
- ),
+  floatingActionButton: FloatingActionButton(
+    onPressed: () {
+    setState(cliqueDoBotao);
+  },
+    child: Icon(Icons.add),
+  ),
  );
  }
 }
